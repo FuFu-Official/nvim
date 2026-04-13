ee07a2dd0c4b8b9b03ae1330bd56755e03a5534b
fufu <fuhongrui15@gmail.com>
2026-04-13 21:10:14 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,15 +10,27 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode using 'jk'" })
 -- Exit visual mode using "q"
 keymap.set("v", "q", "<Esc>", { silent = true, desc = "Exit visual mode" })
 
+-- U for redo
+keymap.set("n", "U", "<C-r>", { desc = "Redo last undone change" })
+
 -- Move selected lines up/down
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
 
 -- Join lines without moving the cursor
-keymap.set("n", "J", "mzJ`z", { desc = "Join line without cursor jump" })
+-- keymap.set("n", "J", "mzJ`z", { desc = "Join line without cursor jump" })
+keymap.set("n", "J", function()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  vim.cmd("normal! J")
+  vim.api.nvim_win_set_cursor(0, pos)
+end, { desc = "Join line without cursor jump" }) -- cleaner than the previous version
 
 ---When pasting a selection, keep the text
 keymap.set("x", "p", [["_dP]])
 
 -- Delete a character without affecting default registers
 keymap.set("n", "x", '"_x', { desc = "Delete character without copying" })
+
+-- Increment/decrement numbers under the cursor
+keymap.set("n", "+", "<C-a>", { desc = "Increment number under cursor" })
+keymap.set("n", "-", "<C-x>", { desc = "Decrement number under cursor" })
---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "nickjvandyke/opencode.nvim",
+    version = "*", -- Latest stable release
+    dependencies = {
+      {
+        -- `snacks.nvim` integration is recommended, but optional
+        ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+        "folke/snacks.nvim",
+        optional = true,
+        opts = {
+          input = {}, -- Enhances `ask()`
+          picker = { -- Enhances `select()`
+            actions = {
+              opencode_send = function(...)
+                return require("opencode").snacks_picker_send(...)
+              end,
+            },
+            win = {
+              input = {
+                keys = {
+                  ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        lsp = { enabled = true },
+        -- Your configuration, if any; goto definition on the type or field for details
+      }
+
+      vim.o.autoread = true -- Required for `opts.events.reload`
+
+      -- Recommended/example keymaps
+      vim.keymap.set({ "n", "x" }, "<leader>as", function()
+        require("opencode").ask("@this: ", { submit = true })
+      end, { desc = "Ask opencode…" })
+      vim.keymap.set({ "n", "x" }, "<leader>ax", function()
+        require("opencode").select()
+      end, { desc = "Execute opencode action…" })
+      vim.keymap.set({ "n", "t" }, "<leader>aa", function()
+        require("opencode").toggle()
+      end, { desc = "Toggle opencode" })
+
+      vim.keymap.set({ "n", "x" }, "go", function()
+        return require("opencode").operator("@this ")
+      end, { desc = "Add range to opencode", expr = true })
+      vim.keymap.set("n", "goo", function()
+        return require("opencode").operator("@this ") .. "_"
+      end, { desc = "Add line to opencode", expr = true })
+
+      vim.keymap.set("n", "<S-C-u>", function()
+        require("opencode").command("session.half.page.up")
+      end, { desc = "Scroll opencode up" })
+      vim.keymap.set("n", "<S-C-d>", function()
+        require("opencode").command("session.half.page.down")
+      end, { desc = "Scroll opencode down" })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_z = opts.sections.lualine_z or {}
+      table.insert(opts.sections.lualine_z, 1, require("opencode").statusline)
+    end,
+  },
+}
